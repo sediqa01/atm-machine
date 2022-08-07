0dcbd8568d7333a2efaf858713a71e41c8c49b49
Sediqa <sadat.hadid2000@gmail.com>
2022-08-07 19:35:15 +0000

    Create User information for login authorization

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ print("*                                            *")
 print("*            Welcome to ATM SYSTEM           *")
 print("*                                            *")
 print("**********************************************")
+
+# Creating lists of users, their PINs and bank statements.
+users = ['sediqa', 'hadid', 'sadat']
+pins = ['0001', '0002', '0003']
+amounts = [10000, 20000, 30000]
+count = 0
