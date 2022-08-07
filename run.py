9f343548e3e9cee32120bc4de3ae3db8109fc98a
Sediqa <sadat.hadid2000@gmail.com>
2022-08-07 19:46:25 +0000

    Check existance of the enterd username

--- run.py
+++ run.py
@@ -15,3 +15,21 @@ users = ['sediqa', 'hadid', 'sadat']
 pins = ['0001', '0002', '0003']
 amounts = [10000, 20000, 30000]
 count = 0
+
+
+# while loop checks existance of the enterd username
+while True:
+    user = input('\nPlease Enter Your  Username: ')
+    user = user.lower()
+
+    if user in users:
+        if user == users[0]:
+            n = 0
+        elif user == users[1]:
+            n = 1
+        else:
+            n = 2
+        break
+    else:
+        print('\n\n     Invalid Username!!')
+
